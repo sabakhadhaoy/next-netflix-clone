8609852a4e8f350d5c3391165ef67f1cb0fadb0d
James delos Reyes <sabakhadhaoy@gmail.com>
2023-05-09 17:45:33 +0800

    info modal component

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
 import { signOut } from 'next-auth/react';
 import React from 'react';
 
@@ -6,6 +7,7 @@ interface AccountMenuProps {
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+   const { data } = useCurrentUser();
    if (!visible) {
       return null;
    }
@@ -14,7 +16,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
          <div className="flex flex-col gap-3">
             <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                <img className="w-8 rounded-md" src="/images/default-red.png" alt="Profile" />
-               <p className="text-white text-sm group-hover/item:underline">Username</p>
+               <p className="text-white text-sm group-hover/item:underline">{data?.name}</p>
             </div>
             <hr className="bg-gray-600 border-0 h-px my-4" />
             <div onClick={() => signOut()} className="px-3 text-center text-white text-sm hover:underline">
